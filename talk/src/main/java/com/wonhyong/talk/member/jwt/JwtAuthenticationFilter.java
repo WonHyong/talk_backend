315f8a928b8a65494c5cadf9484e59c569713697
parkinho <qrdlsgh1234@khu.ac.kr>
2024-01-09 00:34:38 +0900

    EDIT: fix right error

--- talk/src/main/java/com/wonhyong/talk/member/jwt/JwtAuthenticationFilter.java
+++ talk/src/main/java/com/wonhyong/talk/member/jwt/JwtAuthenticationFilter.java
@@ -2,6 +2,7 @@ package com.wonhyong.talk.member.jwt;
 
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.util.StringUtils;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import javax.servlet.FilterChain;
@@ -24,11 +25,18 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
 
         if (token != null && jwtProvider.validateToken(token)) {
             // check access token
-            token = token.split(" ")[1].trim();
             Authentication auth = jwtProvider.getAuthentication(token);
             SecurityContextHolder.getContext().setAuthentication(auth);
         }
 
         filterChain.doFilter(request, response);
+    }
+
+    private String resolveToken(HttpServletRequest request) {
+        String bearerToken = request.getHeader("Authorization");
+        if (StringUtils.hasText(bearerToken) && bearerToken.startsWith("Bearer")) {
+            return bearerToken.substring(7);
+        }
+        return null;
     }
 }
